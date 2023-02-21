fe3019bbfd53fa42584d50e89f28f281554ecd61
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-02-21 17:04:14 -0300

    feat: set default spinnet height

--- src/components/Loading/styled.js
+++ src/components/Loading/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Spinner = styled.img`
-  height: ${( props ) => props.height};
+  height: ${( props ) => props.height ? props.height : "200px"};
 `;
 
 export default Spinner;
